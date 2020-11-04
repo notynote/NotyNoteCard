e256ee3fea1fa949d54ccd9bc156813c13065359
notynote <32334875+notynote@users.noreply.github.com>
2020-11-04 18:24:44 +0700

    Initial Commit

---
+++ NotyNoteCard/NotyNoteCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotyNoteCardApp.swift
+//  NotyNoteCard
+//
+//  Created by Pathompong Phongsaporamut on 4/11/20.
+//
+
+import SwiftUI
+
+@main
+struct NotyNoteCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
