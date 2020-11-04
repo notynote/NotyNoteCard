09e87526a895942130d16c6cb4b38e322f62e544
notynote <32334875+notynote@users.noreply.github.com>
2020-11-04 18:41:43 +0700

    change font and simple layout

--- NotyNoteCard/ContentView.swift
+++ NotyNoteCard/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 0.20, green: 0.60, blue: 0.86)
+                .ignoresSafeArea(.all)
+            VStack {
+                Text("Pathompong Phongsaporamut")
+                    .font(Font.custom("Pacifico-Regular", size: 30))
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
